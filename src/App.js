483d4ea2b8d76924419373cf3731ea9485b276ba
Prasad.barde14 <prasad.barde@gaiansolutions.com>
2023-12-28 15:56:44 +0530

    topleft code part refactor

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div class="parent">
         <div class="video-container video-container1">
          <div class="video-scaler">
-          <iframe src="https://dbaas.netlify.app/" frameborder="0"></iframe>
+          <iframe title='top-left' src="https://dbaas.netlify.app/" frameborder="0" ></iframe>
          </div>
         </div>
     </div>
